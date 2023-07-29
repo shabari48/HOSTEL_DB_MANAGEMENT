1b7c418d2e8642fa48a97dd80ad460c1809b76ec
Shabari <shabarisvg13@gmail.com>
2023-07-29 21:05:10 +0530

    initial db push with docs

---
+++ hdb.sql
@@ -0,0 +1,18 @@
+
+INSERT INTO staff (staff_id, name, category, mob_no, salary) VALUES
+('STF001', 'Rajesh Kumar', 'office', '9876543210', 15000),
+('STF002', 'Sneha Sharma', 'cleaning', '8765432109', 12000),
+('STF003', 'Amit Singh', 'mess', '7654321098', 18000),
+('STF004', 'Neha Patel', 'office', '6543210987', 20000),
+('STF005', 'Vikram Sharma', 'office', '5432109876', 22000),
+('STF006', 'Priya Gupta', 'cleaning', '4321098765', 10000),
+('STF007', 'Anil Kumar', 'office', '3210987654', 23000),
+('STF008', 'Smita Verma', 'mess', '2109876543', 17000),
+('STF009', 'Rahul Sharma', 'office', '1098765432', 24000),
+('STF010', 'Pooja Mehta', 'cleaning', '9876543210', 11000),
+('STF011', 'Arun Singh', 'mess', '8765432109', 19000),
+('STF012', 'Rekha Reddy', 'office', '7654321098', 21000),
+('STF013', 'Ravi Kapoor', 'office', '6543210987', 25000),
+('STF014', 'Meena Patel', 'cleaning', '5432109876', 10000),
+('STF015', 'Sanjay Gupta', 'office', '4321098765', 22000),
+('STF016', 'Kavita Sharma', 'mess', '3210987654', 17000);
